2a75afabcb1da7293d2bf34f52126446094853a9
MistaWizard <tylermckellar@gmail.com>
2018-12-10 20:41:23 -0500

    working game and score

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,16 +38,16 @@ database.ref("/players/").on("value", function(snapshot) {
 
 		// Record player1 data
 		player1 = snapshot.val().player1;
-		// player1Name = player1.name;
+		user1Name = player1.name;
 
 		// // Update player1 display
 		// $("#playerOneName").text(player1Name);
-		// $("#player1Stats").html("Win: " + player1.win + ", Loss: " + player1.loss + ", Tie: " + player1.tie);
+		$("#player1Stats").html("Win: " + player1.win + ", Loss: " + player1.lose + ", Tie: " + player1.tie);
 	} else {
 		console.log("Player 1 does NOT exist");
 
-		// player1 = null;
-		// player1Name = "";
+		player1 = null;
+		user1Name = "";
 
 		// // Update player1 display
 		// $("#playerOneName").text("Waiting for Player 1...");
@@ -56,7 +56,7 @@ database.ref("/players/").on("value", function(snapshot) {
 		// database.ref("/outcome/").remove();
 		// $("#roundOutcome").html("Rock-Paper-Scissors");
 		// $("#waitingNotice").html("");
-		// $("#player1Stats").html("Win: 0, Loss: 0, Tie: 0");
+		$("#player1Stats").html("Win: 0, Loss: 0, Tie: 0");
 	}
 
 	// Check for existence of player 2 in the database
@@ -65,16 +65,16 @@ database.ref("/players/").on("value", function(snapshot) {
 
 		// Record player2 data
 		player2 = snapshot.val().player2;
-		// player2Name = player2.name;
+		user2Name = player2.name;
 
 		// // Update player2 display
 		// $("#playerTwoName").text(player2Name);
-		// $("#player2Stats").html("Win: " + player2.win + ", Loss: " + player2.loss + ", Tie: " + player2.tie);
+		$("#player2Stats").html("Win: " + player2.win + ", Loss: " + player2.lose + ", Tie: " + player2.tie);
 	} else {
 		console.log("Player 2 does NOT exist");
 
-		// player2 = null;
-		// player2Name = "";
+		player2 = null;
+		user2Name = "";
 
 		// // Update player2 display
 		// $("#playerTwoName").text("Waiting for Player 2...");
@@ -83,7 +83,7 @@ database.ref("/players/").on("value", function(snapshot) {
 		// database.ref("/outcome/").remove();
 		// $("#roundOutcome").html("Rock-Paper-Scissors");
 		// $("#waitingNotice").html("");
-		// $("#player2Stats").html("Win: 0, Loss: 0, Tie: 0");
+		$("#player2Stats").html("Win: 0, Loss: 0, Tie: 0");
 	}
 
 	// If both players are now present, it's player1's turn
@@ -98,7 +98,7 @@ database.ref("/players/").on("value", function(snapshot) {
 	// If both players leave the game, empty the chat session
 	if (!player1 && !player2) {
 		// database.ref("/chat/").remove();
-		// database.ref("/turn/").remove();
+		database.ref("/turn/").remove();
 		// database.ref("/outcome/").remove();
 
 		// $("#chatDisplay").empty();
